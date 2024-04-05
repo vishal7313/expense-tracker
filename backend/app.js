92a50e8f96af683717b725a050ac4802685705c0
Vishal <vishal.ranjan7777@gmail.com>
2024-04-04 22:43:11 -0500

    Connect project with mongoose database

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { db } = require('./db/db');
 
 const app = express();
 require('dotenv').config();
@@ -17,6 +18,7 @@ app.use(cors());
 
 // Routes
 const server = () => {
+    db()
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     })
