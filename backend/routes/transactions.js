37f5b608ec8f27d4450adcd22fa833dfc3b7565a
Vishal <vishal.ranjan7777@gmail.com>
2024-04-09 21:20:30 -0500

    Add route to post the income object

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,7 +1,7 @@
+const { addIncome } = require("../controllers/income");
+
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+router.post('/add-income', addIncome);
 
 module.exports = router;
