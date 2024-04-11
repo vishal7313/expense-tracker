e0226b5cb4ac6e0303e8553c0271a599f200ecc8
Vishal <vishal.ranjan7777@gmail.com>
2024-04-10 19:11:13 -0500

    join database and income model

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -1,3 +1,33 @@
+const IncomeSchema = require("../models/incomeModel");
+
 exports.addIncome = async (req, res) => {
-    console.log(req.body);
+    const { title, amount, category, description, date } = req.body;
+
+    const income = new IncomeSchema({
+        title,
+        amount,
+        category,
+        description,
+        date
+    })
+
+    try {
+        // validations
+        if (!title || !amount || !category || !description || !date) {
+            return res.status(400).json({ error: "All fields are required" });
+        }
+
+        if (amount <= 0) {
+            return res.status(400).json({ error: "Amount must be a positive number" });
+        }
+
+        await income.save();
+
+        res.status(201).json({ message: "Income added successfully" });
+
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+
+    console.log(income);
 }
