fd47a60ac485840cc1d0f7bf027ca2a1ab9e4028
Vishal <vishal.ranjan7777@gmail.com>
2024-04-11 18:23:20 -0500

    Endpoints to delete and find incomes

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -31,3 +31,25 @@ exports.addIncome = async (req, res) => {
 
     console.log(income);
 }
+
+exports.getIncomes = async (req, res) => {
+    try {
+        const incomes = await IncomeSchema.find().sort({createdAt: -1});
+        res.status(200).json(incomes);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+exports.deleteIncome = async (req, res) => {
+    const {id} = req.params;
+
+    IncomeSchema.findByIdAndDelete(id)
+        .then((income) => {
+            res.status(200).json({ message: "Income deleted successfully" });
+        })
+        .catch((error) => {
+            res.status(500).json({ error: "Internal Server Error" }); 
+        })
+    
+}
