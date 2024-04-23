6366c99f3614da29c52557cb5f0c245f204cdf80
Vishal <vishal.ranjan7777@gmail.com>
2024-04-22 22:05:01 -0500

    Add header css for expense and income page

--- frontend/src/Components/Expenses/Expenses.jsx
+++ frontend/src/Components/Expenses/Expenses.jsx
@@ -7,13 +7,35 @@ function Expenses() {
         <ExpensesStyle>
             <InnerLayout>
                 <h1>Expenses</h1>
+                <h2 className="total-expense">
+                    Total Expense: <span>$10</span>
+                </h2>
             </InnerLayout>
         </ExpensesStyle>
     )
 }
 
 const ExpensesStyle = styled.div`
-
+    display: flex;
+    overflow: auto;
+    .total-expense {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #FCF6F9;
+        border: 2px solid #FFFFFF;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        border-radius: 20px;
+        padding: 1rem;
+        margin: 1rem 0;
+        font-size: 2rem;
+        gap: .5rem;
+        span {
+            font-size: 2.5rem;
+            font-weight: 800;
+            color: var(--color-green);
+        }
+    }
 `;
 
 export default Expenses
