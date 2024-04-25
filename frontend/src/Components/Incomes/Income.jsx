444873aa9f6554908fb0fe8b947b299e986dbeb3
Vishal <vishal.ranjan7777@gmail.com>
2024-04-24 22:37:16 -0500

    Add income item component to display a section for all the incomes

--- frontend/src/Components/Incomes/Income.jsx
+++ frontend/src/Components/Incomes/Income.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { InnerLayout } from '../../styles/Layouts';
 import { useGlobalContext } from '../../context/globalContext';
 import Form from '../Form/Form';
+import IncomeItem from '../IncomeItem/IncomeItem';
 
 function Income() {
     const {addIncome, getIncomes, incomes} = useGlobalContext();
@@ -24,6 +25,20 @@ function Income() {
                         <Form />
                     </div>
                     <div className='incomes'>
+                        {incomes.map((income) => {
+                            const {_id, title, amount, date, category, description, type} = income;
+                            return <IncomeItem
+                                key={_id}
+                                id={_id} 
+                                title={title} 
+                                description={description} 
+                                amount={amount} 
+                                date={date} 
+                                type={type}
+                                category={category} 
+                                indicatorColor="var(--color-green)"
+                            />
+                        })}
                     </div>
                 </div>
             </InnerLayout>
@@ -34,22 +49,11 @@ function Income() {
 const IncomeStyle = styled.div`
     display: flex;
     overflow: auto;
-    .total-income {
+    .income-content {
         display: flex;
-        justify-content: center;
-        align-items: center;
-        background: #FCF6F9;
-        border: 2px solid #FFFFFF;
-        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
-        border-radius: 20px;
-        padding: 1rem;
-        margin: 1rem 0;
-        font-size: 2rem;
-        gap: .5rem;
-        span {
-            font-size: 2.5rem;
-            font-weight: 800;
-            color: var(--color-green);
+        gap: 2rem;
+        .incomes {
+            flex: 1;
         }
     }
 `;
