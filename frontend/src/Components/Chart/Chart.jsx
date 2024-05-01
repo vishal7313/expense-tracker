be65b29e133aaf6a867e3706ab8b7fc9fe64363d
Vishal <vishal.ranjan7777@gmail.com>
2024-04-30 20:47:43 -0500

    Finish working on showing chart for expense and income

--- frontend/src/Components/Chart/Chart.jsx
+++ frontend/src/Components/Chart/Chart.jsx
@@ -28,9 +28,40 @@ ChartJs.register(
 )
 
 function Chart() {
+    const { incomes, expenses } = useGlobalContext()
+    const data = {
+        labels: incomes.map((inc) =>{
+            const {date} = inc
+            return dateFormat(date)
+        }),
+        datasets: [
+            {
+                label: 'Income',
+                data: [
+                    ...incomes.map((income) => {
+                        const {amount} = income
+                        return amount
+                    })
+                ],
+                backgroundColor: 'green',
+                tension: .2
+            },
+            {
+                label: 'Expenses',
+                data: [
+                    ...expenses.map((expense) => {
+                        const {amount} = expense
+                        return amount
+                    })
+                ],
+                backgroundColor: 'red',
+                tension: .2
+            }
+        ]
+    }
     return (
         <ChartStyled>
-            <Line />
+            <Line data={data}/>
         </ChartStyled>
     )
 }
--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { InnerLayout } from '../../styles/Layouts';
+import Chart from '../Chart/Chart';
 
 function Dashboard() {
     return (
@@ -9,7 +10,7 @@ function Dashboard() {
                 <h1>Dashboard</h1>
                 <div className="stats-con">
                     <div className="chart-con">
-                        
+                        <Chart/>
                     </div>
                 </div>
             </InnerLayout>
